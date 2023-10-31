f12cf6003c41b1a48530894099813caa3e9533ed
Benjamin J. Young <ben@blaher.me>
2023-10-31 10:26:07 -0400

    Removing users routes

--- app/routes.php
+++ app/routes.php
@@ -19,9 +19,4 @@ return function (App $app) {
         $response->getBody()->write('Hello world!');
         return $response;
     });
-
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-    });
 };
