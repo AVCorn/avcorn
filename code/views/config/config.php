d1c5714099777e6943288b1b4bb43b19ad91adcc
Benjamin J. Young <ben@blaher.me>
2023-11-22 14:44:46 -0500

    organizing themes

--- code/views/config/config.php
+++ code/views/config/config.php
@@ -6,10 +6,12 @@ require_once('map.php');
 require_once('navigation.php');
 require_once('navigation-footer.php');
 require_once('information.php');
+require_once('themes.php');
 
 $config = [
   'map' => $map,
   'navigation' => $navigation,
   'navigationFooter' => $navigationFooter,
   'info' => $information,
+  'themes' => $themes,
 ];
---
+++ code/views/config/themes.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+$themes = [
+  'default' => 'default',
+
+  'marketing' => 'examples/marketing',
+  'modern' => 'examples/modern',
+  'portfolio' => 'examples/portfolio',
+  'simple' => 'examples/simple',
+
+  'autoshop' => 'businesses/autoshop',
+  'church' => 'businesses/church',
+  'coffeeshop' => 'businesses/coffeeshop',
+  'restaurant' => 'businesses/restaurant',
+];
