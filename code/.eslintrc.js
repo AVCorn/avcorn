dde629e8021d5ba280cc86e0c0a72eed057ebde9
Benjamin J. Young <ben@blaher.me>
2023-12-10 13:33:25 -0500

    ONE LAST TIME

--- code/.eslintrc.js
+++ code/.eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'standard',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  ignorePatterns: ['/node_modules/*', '/vendor/*'],
+  rules: {
+  }
 }
