d0a62f4ab02b3195f072b2a0379081ead5da1474
Benjamin J. Young <ben@blaher.me>
2023-12-10 14:18:05 -0500

    User Bug

--- code/backend/includes/repositories.php
+++ code/backend/includes/repositories.php
@@ -5,8 +5,5 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
-    // Here we map our UserRepository interface to its in memory implementation
-    $containerBuilder->addDefinitions([
-        UserRepository::class => \DI\autowire(InMemoryUserRepository::class),
-    ]);
+    return;
 };
