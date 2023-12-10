3f3fa3cb8dd7ceb638dcfb4f61dbcf063b50006d
Benjamin J. Young <ben@blaher.me>
2023-12-10 17:20:30 -0500

    deployment aids

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,9 @@ awsregion=${awsregion:-"us-east-2"}
 branch=$(git symbolic-ref -q HEAD)
 branch=${branch##refs/heads/}
 branch=${branch:-HEAD}
-branch=${branch//[\/]/-}
+repo=${branch//[\/]/-}
+cluster=${repo}
+service=${repo}
 
 SCRIPT_DIRECTORY="$(dirname $(realpath "$0"))"
 bash "$SCRIPT_DIRECTORY/build.sh"
@@ -18,6 +20,10 @@ docker tag avcorn "$awsid.dkr.ecr.$awsregion.amazonaws.com/$branch"
 
 aws ecr get-login-password --region "$awsregion" | docker login --username AWS --password-stdin "$awsid.dkr.ecr.$awsregion.amazonaws.com"
 
-docker push "$awsid.dkr.ecr.$awsregion.amazonaws.com/$branch"
+aws ecr create-repository --repository-name "$repo" --region "$awsregion"
+
+docker push "$awsid.dkr.ecr.$awsregion.amazonaws.com/$repo"
+
+aws ecs update-service --cluster "$cluster" --service "$service" --force-new-deployment
 
 echo "Deployment Complete!"
