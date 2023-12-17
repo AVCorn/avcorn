8bcef9b72e364c03d570daac8020cf231f03795a
Benjamin J. Young <ben@blaher.me>
2023-12-17 13:51:54 -0500

    Sitemap restructure

--- code/frontend/templates/_default/config/sitemap.php
+++ code/frontend/templates/_default/config/sitemap.php
@@ -3,17 +3,113 @@
 declare(strict_types=1);
 
 $sitemap = [
-    '/' => 'Home',
-    '/about' => 'About Us',
-    '/guide' => 'Guide',
-    '/designs' => 'Designs',
-    '/gallery' => 'Gallery',
-    '/playground' => 'Styles Playground',
-    '/services' => 'Services',
-    '/testimonials' => 'Testimonials',
-    '/contact' => 'Contact Us',
-    '/contribute' => 'Contribute',
-    '/terms-of-use' => 'Terms of Use',
-    '/privacy-policy' => 'Privacy Policy',
-    '/sitemap' => 'Sitemap',
+    'sections' => [
+        'main' => [
+            'title' => 'AVCorn',
+            'link' => '/',
+            'pages' => [
+                'home',
+                'about',
+                'services',
+                'gallery',
+                'testimonials',
+                'contact',
+            ],
+        ],
+        'secondary' => [
+            'title' => 'Resources',
+            'description' => 'Find out more about us.',
+            'link' => '/about',
+            'pages' => [
+                'guide',
+                'designs',
+                'playground',
+                'contribute',
+            ],
+        ],
+        'other' => [
+            'title' => 'Other Details',
+            'description' => 'Additional information.',
+            'link' => '/sitemap',
+            'pages' => [
+                'terms',
+                'privacy',
+                'sitemap',
+            ],
+        ],
+    ],
+    'pages' => [
+        'home' => [
+            'title' => 'Home',
+            'link' => '/',
+        ],
+        'about' => [
+            'title' => 'About Us',
+            'description' => 'Information and history of AVCorn.',
+            'link' => '/about',
+        ],
+        'contact' => [
+            'title' => 'Contact Us',
+            'description' => 'How to contact us.',
+            'link' => '/Contact',
+        ],
+        'contribute' => [
+            'title' => 'Contribute',
+            'description' => 'How you can contribute to AVCorn.',
+            'link' => '/contribute',
+        ],
+        'guide' => [
+            'title' => 'Guide',
+            'description' => 'Walkthrough of how to use AVCorn.',
+            'link' => '/guide',
+        ],
+        'designs' => [
+            'title' => 'Designs',
+            'description' => 'List of our designs.',
+            'link' => '/designs',
+        ],
+        'gallery' => [
+            'title' => 'Gallery',
+            'description' => 'Photo gallery.',
+            'link' => '/galley',
+        ],
+        'playground' => [
+            'title' => 'Styles Playground',
+            'description' => 'Everything on demo, but the kitchen sink.',
+            'link' => '/playground',
+        ],
+        'services' => [
+            'title' => 'Services',
+            'description' => 'Services we offer.',
+            'link' => '/services',
+        ],
+        'testimonials' => [
+            'title' => 'Testimonials',
+            'description' => 'Testimonials of our customers.',
+            'link' => '/testimonials',
+        ],
+        'terms' => [
+            'title' => 'Terms of Use',
+            'description' => 'Terms of use of the website.',
+            'link' => '/terms-of-use',
+        ],
+        'privacy' => [
+            'title' => 'Privacy Policy',
+            'description' => 'Privacy policy of the website.',
+            'link' => '/privacy-policy',
+        ],
+        'sitemap' => [
+            'title' => 'Sitemap',
+            'description' => 'Navigation map of the website.',
+            'link' => '/sitemap',
+        ],
+        '404' => [
+            'title' => 'Page Not Found',
+            'link' => '/404',
+        ],
+        '500' => [
+            'title' => 'Internal Server Error',
+            'link' => '/500',
+        ],
+    ],
 ];
