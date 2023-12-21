20090dba1dd69e15a7ded091cb821a95dc032ead
Benjamin J. Young <ben@blaher.me>
2023-12-21 10:18:44 -0500

    Scrolling banner on webelements

--- code/frontend/templates/clients/webelements/config/navigation.php
+++ code/frontend/templates/clients/webelements/config/navigation.php
@@ -13,18 +13,18 @@ declare(strict_types=1);
 $navigation = [
     [
         'href' => '/about',
-        'label' => '<i class="fa fa-users"></i> Who We Are',
+        'label' => '<i class="show-for-medium fa fa-users"></i> Who We Are',
     ],
     [
         'href' => '/clients',
-        'label' => '<i class="fa fa-briefcase"></i> Our Work',
+        'label' => '<i class="show-for-medium fa fa-briefcase"></i> Our Work',
     ],
     [
         'href' => '/services',
-        'label' => '<i class="fa fa-globe"></i> What We Offer',
+        'label' => '<i class="show-for-medium fa fa-globe"></i> What We Offer',
     ],
     [
         'href' => '/contact',
-        'label' => '<i class="fa fa-phone"></i> Connect',
+        'label' => '<span class="show-for-medium"><i class="fa fa-phone"></i> Contact Us</span><span class="button show-for-small-only">Connect</span>',
     ],
 ];
