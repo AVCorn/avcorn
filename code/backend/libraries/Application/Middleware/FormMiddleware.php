9d2015e3e64578569efb6ba0fe783a8adbe25f8e
Benjamin J. Young <ben@blaher.me>
2023-12-23 09:07:37 -0500

    Refactoring to prepare for emails on forms and separate form classes

--- code/backend/libraries/Application/Middleware/FormMiddleware.php
+++ code/backend/libraries/Application/Middleware/FormMiddleware.php
@@ -37,54 +37,73 @@ class FormMiddleware implements Middleware
 
         // if a form is submitted
         if (isset($params['form']) && $params['form'] == 1) {
-            // set sheet info
-            $spreadsheetId = $params['form_id'];
-            unset($params['form_id']);
-            $range = $params['form_range'] ? $params['form_range'] : 'Sheet1';
-            unset($params['form_range']);
+            // set status for later
+            $status = 2;
 
-            // configure the Google Client
-            $client = new Google_Client();
-            $client->setAuthConfig(__DIR__ . '/../../../../_env/google.private.key.json');
-            $client->setApplicationName('Google Sheets API');
-            $client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
+            // TODO: split up cases in to seperate classes
 
-            // configure the Sheets Service
-            $service = new Google_Service_Sheets($client);
+            // for SES emails
+            if (isset($params['form_email_to'])) {
+                // email here using SES
+            }
 
-            // loop through $params
-            foreach ($params as $key => $value) {
-                // if the key is not a 'form-', unset it
-                if (substr($key, 0, 5) !== 'form_') {
-                    unset($params[$key]);
+            // for google sheets
+            if (isset($params['form_sheet_id'])) {
+                // set sheet info
+                $spreadsheetId = $params['form_sheet_id'];
+                unset($params['form_sheet_id']);
+                $range = $params['form_sheet_range'] ? $params['form_sheet_range'] : 'Sheet1';
+                unset($params['form_sheet_range']);
+
+                // configure the Google Client
+                $client = new Google_Client();
+                $client->setAuthConfig(__DIR__ . '/../../../../_env/google.private.key.json');
+                $client->setApplicationName('Google Sheets API');
+                $client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
+
+                // configure the Sheets Service
+                $service = new Google_Service_Sheets($client);
+
+                // loop through $params
+                foreach ($params as $key => $value) {
+                    // if the key is not a 'form-', unset it
+                    if (substr($key, 0, 5) !== 'form_') {
+                        unset($params[$key]);
+                    }
+                }
+
+                // sort $params alphabetically
+                ksort($params);
+
+                // replace all keys of $params with integers
+                $params = array_values($params);
+
+                // build rows to insert
+                $rows = [$params]; // you can append several rows at once
+                $valueRange = new Google_Service_Sheets_ValueRange();
+                $valueRange->setValues($rows);
+                $options = ['valueInputOption' => 'USER_ENTERED'];
+
+                // insert the row
+                try {
+                    $service->spreadsheets_values->append($spreadsheetId, $range, $valueRange, $options);
+                } catch (\Exception $e) {
+                    $status = 0;
                 }
             }
 
-            // sort $params alphabetically
-            ksort($params);
 
-            // replace all keys of $params with integers
-            $params = array_values($params);
-
-            // build rows to insert
-            $rows = [$params]; // you can append several rows at once
-            $valueRange = new Google_Service_Sheets_ValueRange();
-            $valueRange->setValues($rows);
-            $options = ['valueInputOption' => 'USER_ENTERED'];
-
-            $status = 2;
-            try {
-                // insert the row
-                $service->spreadsheets_values->append($spreadsheetId, $range, $valueRange, $options);
-
+            if ($status > 1) {
                 // get the request URI and redirect
                 $uri = (string)$request->getUri()->withQuery($request->getUri()->getQuery() . '&form=' . $status);
                 return $handler->handle($request)->withHeader('Location', $uri)->withStatus(302);
-            } catch (\Exception $e) {
+            } else {
+                // continue to load form to handle erro
                 return $handler->handle($request);
             }
         }
 
+        // no form handling was found, so continue
         return $handler->handle($request);
     }
 }
